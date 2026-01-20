6a8e155e79ac81a347691e113ba77bc718af4729
Rumeshi Bandara <rumeshicharuni@gmail.com>
2026-01-20 14:03:36 +0530

    add to jpaRepository to eventRepository

--- src/main/java/org/example/repository/EventRepository.java
+++ src/main/java/org/example/repository/EventRepository.java
@@ -1,4 +1,8 @@
 package org.example.repository;
 
-public class EventRepository {
+
+import org.example.model.entity.Event;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EventRepository extends JpaRepository<Event, Long> {
 }
