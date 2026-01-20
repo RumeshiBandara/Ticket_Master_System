67471f4d087f80e56c449fabbaf7599423584ae7
Rumeshi Bandara <rumeshicharuni@gmail.com>
2026-01-20 14:11:26 +0530

    complete repository package

--- src/main/java/org/example/repository/BookingRepository.java
+++ src/main/java/org/example/repository/BookingRepository.java
@@ -1,4 +1,7 @@
 package org.example.repository;
 
-public class BookingRepository {
+import org.example.model.entity.Booking;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BookingRepository extends JpaRepository<Booking, Long> {
 }
